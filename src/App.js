37fe3f63c29cfac164733e4e989aa1150dc7732f
uy154788 <118651983+uy154788@users.noreply.github.com>
2024-07-15 23:17:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[text,setText]=useState('');
+
+  function changeHandler(event){
+    console.log(text);
+    setText(event.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={changeHandler}></input>
     </div>
   );
 }
